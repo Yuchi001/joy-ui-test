72e65cdc71094d6ef4e4146c97c7a9296f4008f5
Kamil <kamil.antos@qsystems.pro>
2023-10-26 15:43:54 +0200

    Created sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@fontsource/inter';
+import {DefaultRoot} from "./Routing/DefaultRoot/DefaultRoot";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import {CssVarsProvider} from "@mui/joy";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultRoot/>
+    },
+]);
+
+root.render(
+  <CssVarsProvider>
+      <RouterProvider router={router} />
+  </CssVarsProvider>
+);
